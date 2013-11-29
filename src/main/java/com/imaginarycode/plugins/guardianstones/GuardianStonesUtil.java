5069de41c2a43daf7e7319e2743f6e5b9225b333
Tux <write@imaginarycode.com>
2013-11-29 16:23:30 -0500

    Reformatting.

--- src/main/java/com/imaginarycode/plugins/guardianstones/GuardianStonesUtil.java
+++ src/main/java/com/imaginarycode/plugins/guardianstones/GuardianStonesUtil.java
@@ -1,49 +1,46 @@
 package com.imaginarycode.plugins.guardianstones;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
-
 import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
- * 
  * @author tux-amd64
- *
  */
 public class GuardianStonesUtil {
-	public static YamlConfiguration loadYaml(String file) {
-    	File map = new File(GuardianStones.self.getDataFolder(), file);
-    	if(!map.exists()) {
-    		GuardianStones.self.getLogger().info(file + " not found, creating...");
-    		try {
-    			GuardianStones.self.getDataFolder().mkdirs();
-				map.createNewFile();
-				GuardianStones.self.getLogger().info("File created successfully!");
-			} catch (IOException e) {
-				GuardianStones.self.getLogger().info("Unable to create " + file + ", this may be harmless:");
-				e.printStackTrace();
-			}
-    	}
-    	return YamlConfiguration.loadConfiguration(map);
-	}
-	
-	public static void saveYaml(YamlConfiguration y, String file) {
-    	File map = new File(GuardianStones.self.getDataFolder(), file);
-    	try {
-    		GuardianStones.self.getDataFolder().mkdirs();
-    		y.save(map);
-		} catch (IOException e) {
-			GuardianStones.self.getLogger().info("Unable to create " + file + ", this may be harmless:");
-			e.printStackTrace();
-    	}
-	}
-	
-    public static String getLocationAsStringGuid(Location loc) {
-    	return "_" + loc.getBlockX() + "_" + loc.getBlockY() + "_" + loc.getBlockZ();
+    public static YamlConfiguration loadYaml(String file) {
+        File map = new File(GuardianStones.self.getDataFolder(), file);
+        if (!map.exists()) {
+            GuardianStones.self.getLogger().info(file + " not found, creating...");
+            try {
+                GuardianStones.self.getDataFolder().mkdirs();
+                map.createNewFile();
+                GuardianStones.self.getLogger().info("File created successfully!");
+            } catch (IOException e) {
+                GuardianStones.self.getLogger().info("Unable to create " + file + ", this may be harmless:");
+                e.printStackTrace();
+            }
+        }
+        return YamlConfiguration.loadConfiguration(map);
     }
-    
+
+    public static void saveYaml(YamlConfiguration y, String file) {
+        File map = new File(GuardianStones.self.getDataFolder(), file);
+        try {
+            GuardianStones.self.getDataFolder().mkdirs();
+            y.save(map);
+        } catch (IOException e) {
+            GuardianStones.self.getLogger().info("Unable to create " + file + ", this may be harmless:");
+            e.printStackTrace();
+        }
+    }
+
+    public static String getLocationAsStringGuid(Location loc) {
+        return "_" + loc.getBlockX() + "_" + loc.getBlockY() + "_" + loc.getBlockZ();
+    }
+
     public static String serializedLoc(Location loc) {
         return "" + loc.getBlockX() + "," + Math.round(loc.getY()) + "," + loc.getBlockZ();
     }
--- src/main/java/com/imaginarycode/plugins/guardianstones/protectionplugins/ProtectionPlugin.java
+++ src/main/java/com/imaginarycode/plugins/guardianstones/protectionplugins/ProtectionPlugin.java
@@ -1,100 +1,99 @@
 package com.imaginarycode.plugins.guardianstones.protectionplugins;
 
+import com.imaginarycode.plugins.guardianstones.GuardianStonesException;
 import com.imaginarycode.plugins.guardianstones.ProtectionFlag;
 import org.bukkit.Location;
 
-import com.imaginarycode.plugins.guardianstones.GuardianStonesException;
-
 /**
  * This class implements the guideline for an protection plugin to be supported by GuardianStones.
- * @author tux-amd64
  *
+ * @author tux-amd64
  */
 public interface ProtectionPlugin {
-	/**
-	 * Enable this protection plugin.
-	 * 
-	 * The plugin's main class is available via GuardianStones.self.
-	 * 
-	 * @return true if enabled; false if not
-	 */
-	boolean enable();
-	
-	/**
-	 * Disable this protection plugin.
-	 * 
-	 * The plugin's main class is available via GuardianStones.self.
-	 * 
-	 * @return true if disabled; false if not
-	 */
-	boolean disable();
-	
-	/**
-	 * Check if this Location has any protections (placed by GuardianStones)
-	 * whatsoever.
-	 * 
-	 * @param loc
-	 * @param guardianStonesOnly
-	 * @return true if protected by GuardianStones
-	 */
-	boolean isProtected(Location loc, boolean guardianStonesOnly);
-	
-	/**
-	 * Get the region of the protection.
-	 * 
-	 * @param loc
-	 * @return region name
-	 */
-	String getRegion(Location loc);
-	
-	/**
-	 * Add a member to a region.
-	 * 
-	 * @param regionName
-	 * @param player
-	 * @return true if succeeded
-	 * @throws GuardianStonesException if the addition failed
-	 */
-	boolean addMemberToRegion(String regionName, String player) throws GuardianStonesException;
-	
-	/**
-	 * Remove a member from a region.
-	 * 
-	 * @param regionName
-	 * @param player
-	 * @return true if succeeded
-	 * @throws GuardianStonesException if the removal failed
-	 */
-	boolean removeMemberFromRegion(String regionName, String player) throws GuardianStonesException;
-	
-	/**
-	 * Define a region, specified by a Location (the center of the protection),
-	 * an initial owner and a radius. The protection should have:
-	 * <ul>
-	 * <li>Creeper explosions denied
-	 * <li>TNT denied
-	 * <li>Redstone mechanism use denied
-	 * <li>PvP denied
-	 * <li>Mob damage and spawning denied
-	 * <li>No overlap with any other GuardianStones region
-	 * </ul>
-	 * 
-	 * @param owner
-	 * @param center
-	 * @param radius
-	 * @return the name of the region
-	 * @throw GuardianStonesException if the creation failed
-	 */
-	String defineRegion(String owner, Location center, int radius) throws GuardianStonesException;
+    /**
+     * Enable this protection plugin.
+     * <p/>
+     * The plugin's main class is available via GuardianStones.self.
+     *
+     * @return true if enabled; false if not
+     */
+    boolean enable();
 
-	/**
-	 * Remove a region.
-	 * 
-	 * @param name
-	 * @return true if region was removed
-	 * @throw GuardianStonesException if the removal failed
-	 */
-	boolean removeRegion(String name) throws GuardianStonesException;
+    /**
+     * Disable this protection plugin.
+     * <p/>
+     * The plugin's main class is available via GuardianStones.self.
+     *
+     * @return true if disabled; false if not
+     */
+    boolean disable();
+
+    /**
+     * Check if this Location has any protections (placed by GuardianStones)
+     * whatsoever.
+     *
+     * @param loc
+     * @param guardianStonesOnly
+     * @return true if protected by GuardianStones
+     */
+    boolean isProtected(Location loc, boolean guardianStonesOnly);
+
+    /**
+     * Get the region of the protection.
+     *
+     * @param loc
+     * @return region name
+     */
+    String getRegion(Location loc);
+
+    /**
+     * Add a member to a region.
+     *
+     * @param regionName
+     * @param player
+     * @return true if succeeded
+     * @throws GuardianStonesException if the addition failed
+     */
+    boolean addMemberToRegion(String regionName, String player) throws GuardianStonesException;
+
+    /**
+     * Remove a member from a region.
+     *
+     * @param regionName
+     * @param player
+     * @return true if succeeded
+     * @throws GuardianStonesException if the removal failed
+     */
+    boolean removeMemberFromRegion(String regionName, String player) throws GuardianStonesException;
+
+    /**
+     * Define a region, specified by a Location (the center of the protection),
+     * an initial owner and a radius. The protection should have:
+     * <ul>
+     * <li>Creeper explosions denied
+     * <li>TNT denied
+     * <li>Redstone mechanism use denied
+     * <li>PvP denied
+     * <li>Mob damage and spawning denied
+     * <li>No overlap with any other GuardianStones region
+     * </ul>
+     *
+     * @param owner
+     * @param center
+     * @param radius
+     * @return the name of the region
+     * @throw GuardianStonesException if the creation failed
+     */
+    String defineRegion(String owner, Location center, int radius) throws GuardianStonesException;
+
+    /**
+     * Remove a region.
+     *
+     * @param name
+     * @return true if region was removed
+     * @throw GuardianStonesException if the removal failed
+     */
+    boolean removeRegion(String name) throws GuardianStonesException;
 
     /**
      * Toggle a flag.
